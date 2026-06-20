2709d47c49a95996140a9b7f3726f9f5057da7fb
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-20 09:31:15 +0530

    Added pattern 2

---
+++ DSA Patterns ques/pattern2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n ; i++) {
+            for(int j=n ; i<=j ; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

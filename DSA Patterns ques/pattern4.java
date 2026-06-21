a5ff2586e67993bc462db240d7bc81709924e5b2
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-21 08:59:01 +0530

    Added pattern4

---
+++ DSA Patterns ques/pattern4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class pattern4{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int space = 0;
+        int star = n;
+
+         for(int i=1 ; i<=n ; i++){
+           // System.out.print(space + "," + star);
+
+            for(int j=1 ; j<=space ; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1 ; j<=star ; j++){
+                System.out.print("*");
+            }
+
+            space++;
+            star--;
+            System.out.println();
+         }
+    }
+}

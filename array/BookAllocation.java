a6a2c978306abd2f1d84d2af57704497aa8346c9
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:33:24 +0530

    Added Array file

---
+++ array/BookAllocation.java
@@ -0,0 +1,64 @@
+class BookAllocation {
+                                                        // mid ==> maxAllowedPages
+     public static boolean isValid(int[] arr , int n , int m , int maxAllowedPages){ 
+         int students = 1;
+         int pages = 0;
+
+         for(int i = 0; i < n ; i++){
+             if(arr[i] > maxAllowedPages){
+                return false;
+             }
+
+             if(pages + arr[i] <= maxAllowedPages){
+                pages += arr[i];
+             }
+             else {
+                students++;
+                pages = arr[i];
+             }
+         }
+           return students > m ? false : true;
+     }
+ 
+  public static int allocateBooks(int[] arr , int n , int m){
+       
+       if(m > n){
+         return -1;
+       }
+
+     int sum = 0;  // first we are finding min and max values..
+
+      for(int i = 0; i<n ; i++){
+         sum += arr[i];
+      }
+
+      int start = 0;  // range of possible answers
+      int end = sum;
+      int ans = -1;
+
+      while( start <= end){
+        int mid = start + (end - start)/2;
+         
+         // after finding mid , now check validity.... of mid
+          
+          if(isValid(arr , n , m , mid)){     // left sorting
+             ans = mid;
+             end = mid - 1;
+          }
+           else { // right sorting....
+            start = mid + 1;
+           }
+      }
+
+       return ans;
+  }
+   
+    public static void main(String[] args){
+        int[] arr = {2,1,3,4};
+        int n = 4;  // number of pages 
+        int m = 2;  // number of students
+
+         System.out.println(allocateBooks(arr , n , m));
+
+    }
+}
